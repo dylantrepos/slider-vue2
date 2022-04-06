a98e0e93b8c42ad898d6f7acf84c5204950e7bba
Dylan Trepos <dylan.trepos@gmail.com>
2022-04-06 21:35:13 +0200

    test

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <p>Relation Parent/child test : {{ todos }}</p>
+    <TodosApp v-model="todos" @input="updateTodos"></TodosApp>
+    <button @click="addTodo">Ajouter une tache</button>
+  </div>
+</template>
+
+<script>
+import TodosApp from './components/TodosApp.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      todos: [{
+        name: 'Test de todo',
+        completed: false
+      }]
+    }
+  },
+  components: {
+    TodosApp
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        name: 'Jeanjean',
+        completed: false
+      })
+    },
+    updateTodos(newTodos) {
+      this.todos = newTodos;
+    }
+  }
+}
+</script>
